6019b0d2f6c67555908612e33b7b3a564f923a25
Luig Rocha <Luig Rocha@luigrocha>
2015-12-06 23:20:58 -0500

    LuisRocha prueba

---
+++ ProyectoMaldito2/ProyectoMaldito2-ejb/src/main/java/com/espe/distribuidas/model/prueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.espe.distribuidas.model;
+
+/**
+ *
+ * @author Luig Rocha
+ */
+public class prueba {
+    
+}
